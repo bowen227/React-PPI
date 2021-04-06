c7032e7bb299419713c26be8433041460380298e
Brent Owen <brent.e.owen@gmail.com>
2021-04-06 09:53:44 -0400

    Added navigation component

---
+++ src/components/NavComponent.js
@@ -0,0 +1,40 @@
+import { Component } from 'react';
+import { Navbar, NavbarBrand, Collapse, Nav, NavItem, NavLink, DropdownToggle, DropdownMenu, DropdownItem, NavbarToggler } from 'reactstrap';
+
+class Navigation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false
+        }
+    }
+
+    toggle(open) {
+        this.setState({isOpen: !open})
+    }
+
+    render() {
+
+        return(
+            <Navbar color="dark" dark expand="md">
+            <NavbarBrand href="/">PPI-LOGO Here</NavbarBrand>
+            <NavbarToggler onClick={() => this.toggle(this.state.isOpen)} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                    <NavItem>
+                      <NavLink href="/">For Associations</NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink href="/">For Coaches</NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink href="/">Login</NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+          </Navbar>
+        )
+    }
+}
+
+export default Navigation;
