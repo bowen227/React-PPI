33e101026740490c96b9482d68aa8d9a00f3e8c7
Brent Owen <brent.e.owen@gmail.com>
2021-04-09 16:48:12 -0400

    Added player data and created list for players in the dashboard

---
+++ src/shared/players.js
@@ -0,0 +1,12 @@
+export const PLAYERS = [
+    { firstName: 'Jason', lastName: 'Owen', group: '6u', bats: 'right', throws: 'right' },
+    { firstName: 'Tim', lastName: 'Ledford', group: '6u', bats: 'right', throws: 'right' },
+    { firstName: 'Mike', lastName: 'Dick', group: '6u', bats: 'right', throws: 'right' },
+    { firstName: 'Brent', lastName: 'Lyons', group: '6u', bats: 'right', throws: 'right' },
+    { firstName: 'Jay', lastName: 'Wingo', group: '6u', bats: 'right', throws: 'right' },
+    { firstName: 'Easton', lastName: 'Morrow', group: '6u', bats: 'right', throws: 'right' },
+    { firstName: 'Donald', lastName: 'Wilson', group: '6u', bats: 'right', throws: 'right' },
+    { firstName: 'Brandon', lastName: 'Gibson', group: '6u', bats: 'right', throws: 'right' },
+    { firstName: 'Nick', lastName: 'Malone', group: '6u', bats: 'right', throws: 'right' },
+    { firstName: 'Jarred', lastName: 'Hargis', group: '6u', bats: 'right', throws: 'right' },
+]
--- src/views/DashboardComponent.js
+++ src/views/DashboardComponent.js
@@ -1,11 +1,44 @@
 import { Component } from 'react';
+import { ListGroup, ListGroupItem } from 'reactstrap';
+import { PLAYERS } from '../shared/players';
 
 class Dashboard extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            players: PLAYERS
+        }
     }
 
     render() {
+        const playerList = this.state.players.map(player => {
+            return (
+                <ListGroupItem className="player-list">
+                    <div className="row justify-content-center">
+                        <div className="col-1">
+                            <img className="default-user-img" src="./assets/images/default_user.png"></img>
+                        </div>
+                        <div className="col d-flex align-items-center">
+                            {player.firstName + ' ' + player.lastName}
+                        </div>
+                        <div className="col d-flex align-items-center">
+                            {player.throws}
+                        </div>
+                        <div className="col d-flex align-items-center">
+                            {player.bats}
+                        </div>
+                        <div className="col d-flex align-items-center">
+                            {player.group}
+                        </div>
+                        <div className="col d-flex align-items-center">
+                            0.00
+                        </div>
+                    </div>
+                </ListGroupItem>
+            );
+        });
+
         return(
             <div className='container py-5'>
                 <div className="row">
@@ -17,7 +50,9 @@ class Dashboard extends Component {
                     </div>
                 </div>
                 <div className="row py-5">
-                    PlayerListComponent goes here
+                    <ListGroup flush className="w-100">
+                        {playerList}
+                    </ListGroup>
                 </div>
             </div>
         )
